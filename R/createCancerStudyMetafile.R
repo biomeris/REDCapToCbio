8c591012c284e63292e3aaffa5307c2a193eeb12
Vittoria Ramella <vittoria.ramella@biomeris.it>
2025-04-17 11:31:49 +0200

    Added createCancerStudyMetafile() and createCancerTypefiles() functions

---
+++ R/createCancerStudyMetafile.R
@@ -0,0 +1,42 @@
+#' Create Cancer Study Meta File
+#'
+#' @param study_folder The study folder.
+#' @param type_of_cancer The cancer type abbreviation. This should be the same cancer type as specified in the meta_cancer_type.txt file, if available. The type can be "mixed" for studies with multiple cancer types.
+#' @param cancer_study_identifier A string used to uniquely identify this cancer study within the database.
+#' @param name The name of the cancer study.
+#' @param description A description of the cancer study. This description may contain one or more URLs to relevant information.
+#' @param citation (Optional) A relevant citation.
+#' @param pmid (Optional) One or more relevant pubmed ids (comma separated without whitespace). If used, the field citation has to be filled, too.
+#' @param groups (Optional) When using an authenticating cBioPortal, lists the user-groups that are allowed access to this study. Multiple groups are separated with a semicolon ";". The study will be invisible to users not in at least one of the listed groups, as if it wasn't loaded at all. See [User-Authorization](https://docs.cbioportal.org/deployment/authorization-and-authentication/user-authorization/) for more information on groups
+#' @param add_global_case_list (Optional) Set to 'true' if you would like the "All samples" case list to be generated automatically for you. See also [Case list](https://docs.cbioportal.org/file-formats/#case-lists)
+#' @param tags_file (Optional) The file name containing custom study tags for the [study tags](https://docs.cbioportal.org/file-formats/#study-tags-file).
+#' @param reference_genome (Optional) The study reference genome (e.g. hg19, hg38). Without specifying this property, the study will be assigned to the reference genome specified in application.properties (property ucsc.build).
+#'
+#' @export
+#'
+#' @examples
+#' createCancerTypeFiles(study_folder = tempdir(),
+#'   type_of_cancer = "brca",
+#'   name = "Breast Invasive Carcinoma",
+#'   dedicated_color = "HotPink",
+#'   parent_type_of_cancer = "Breast")
+createCancerStudyMetafile <- function(study_folder, type_of_cancer, cancer_study_identifier, name, description, citation = NULL, pmid= NULL, groups = NULL, add_global_case_list = NULL, tags_file = NULL, reference_genome = NULL) {
+  # Create list with all fields
+  fields_list <- mget(c("type_of_cancer", "cancer_study_identifier", "name", "description", "citation", "pmid", "groups", "add_global_case_list", "tags_file", "reference_genome"), envir = environment())
+
+  # Remove NULL elements
+  fields_list <- fields_list[!sapply(fields_list, is.null)]
+
+  # Convert boolean values to lowercase string
+  fields_list <- lapply(fields_list, function(x) {
+    if (is.logical(x)) return(tolower(as.character(x))) else return(x)
+  })
+
+  # Convert list to array of strings
+  lines <- paste(names(fields_list), fields_list, sep = ": ")
+
+  # Write txt file to output folder
+  writeLines(lines, file.path(study_folder, "meta_study.txt"))
+
+  cat("File 'meta_study.txt' successfully created!\n")
+}
---
+++ R/createCancerTypeFiles.R
@@ -0,0 +1,29 @@
+#' Create Cancer Type Meta File and Data File
+#'
+#' @param study_folder The study folder.
+#' @param data_filename The datafile name. If not specified, default is 'cancer_type.txt'
+#' @param type_of_cancer The cancer type abbreviation.
+#' @param name The name of the cancer type.
+#' @param dedicated_color CSS color name of the color associated with this cancer study. See [this list](https://www.w3.org/TR/css3-color/#svg-color) for supported names, and follow the [awareness ribbons](https://en.wikipedia.org/wiki/List_of_awareness_ribbons) color schema. This color is associated with the cancer study on various web pages within the cBioPortal.
+#' @param parent_type_of_cancer The type_of_cancer field of the cancer type of which this is a subtype. ℹ️ : you can set parent to tissue, which is the reserved word to place the given cancer type at "root" level in the "studies oncotree" that will be generated in the homepage (aka query page) of the portal.
+#'
+#' @export
+#'
+#' @examples
+#' createCancerTypeFiles(study_folder = tempdir(),
+#'   type_of_cancer = "brca",
+#'   name = "Breast Invasive Carcinoma",
+#'   dedicated_color = "HotPink",
+#'   parent_type_of_cancer = "Breast")
+createCancerTypeFiles <- function(study_folder, data_filename = "cancer_type.txt", type_of_cancer, name, dedicated_color, parent_type_of_cancer) {
+  # Write meta data file
+  lines <- c("genetic_alteration_type: CANCER_TYPE", "datatype: CANCER_TYPE", paste("data_filename", data_filename, sep = ": "))
+  writeLines(lines, file.path(study_folder, "meta_cancer_type.txt"))
+  cat("File 'meta_cancer_type.txt' successfully created!\n")
+
+  # Write data file
+  col_list <- mget(c("type_of_cancer", "name", "dedicated_color", "parent_type_of_cancer"), envir = environment())
+  line <- paste(col_list, collapse = "\t")
+  writeLines(line, file.path(study_folder, data_filename))
+  cat(sprintf("File '%s' successfully created!\n", data_filename))
+}
