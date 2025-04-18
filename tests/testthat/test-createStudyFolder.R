559ab45128c77fc457745802750817bedcd3d0e4
Vittoria Ramella <vittoria.ramella@biomeris.it>
2025-04-18 12:27:40 +0200

    Added test for createStudyFolder() function

--- tests/testthat/test-createStudyFolder.R
+++ tests/testthat/test-createStudyFolder.R
@@ -1,3 +1,11 @@
-test_that("create a new folder", {
-  skip()
+test_that("Create a new study folder", {
+  temp_path <- tempdir()
+  studyFolder <- paste0("study_", as.integer(Sys.time()))
+  full_path <- file.path(temp_path, studyFolder)
+
+  on.exit(unlink(full_path, recursive = TRUE))
+
+  createStudyFolder(study_folder = studyFolder, output_path = temp_path)
+
+  expect_true(dir.exists(full_path))
 })
